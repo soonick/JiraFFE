7298998a02ef52ac4a47c6eadea79a9d80988241
Adrian Ancona Novelo <anovelo@linkedin.com>
2013-06-14 16:09:43 -0700

    Toggle button now works

--- js/jiraffe.js
+++ js/jiraffe.js
@@ -1,3 +1,28 @@
+function toggleIcon() {
+  var storage = chrome.storage.local;
+  storage.get('isActive', function(val) {
+    if (val.isActive) {
+      chrome.browserAction.setIcon({ path: 'img/jiraffe.jpg' });
+      storage.set({ 'isActive': false });
+    } else {
+      chrome.browserAction.setIcon({ path: 'img/jiraffe2.jpg' });
+      storage.set({ 'isActive': true });
+    }
+  });
+}
+
+function initializeIcon() {
+  chrome.storage.local.get('isActive', function(val) {
+    if (val.isActive) {
+      chrome.browserAction.setIcon({ path: 'img/jiraffe2.jpg' });
+    } else {
+      chrome.browserAction.setIcon({ path: 'img/jiraffe.jpg' });
+    }
+  });
+}
+
 chrome.browserAction.onClicked.addListener(function(Tab){
-  chrome.browserAction.setIcon({ path: 'img/jiraffe2.jpg'});
+  toggleIcon();
 });
+
+initializeIcon();
